5f67fa711db80913504a291c3dc15f3345d68b61
anjana732 <anjanasingh1257@gmail.com>
2025-06-17 16:23:05 +0530

    Debug mongodb connection error

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
-import { logger } from '../utils/logger';
+import { logger } from '../utils/logger.js';
 
 const connectDB = async ()=>{
     try{
         const resp = await mongoose.connect(process.env.MONGODB_URI);
-        if(resp.OK)  logger.info(`MongoDB Connected: ${conn.connection.host}`);
+        logger.info(`MongoDB Connected: ${resp.connection.host}`);
     }catch(err){
         logger.error(`MongoDB connection error: ${err.message}`,{stack : err.stack});
         process.exit(1);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import app from './app.js'
+import connectDB from './config/db.js'
 import {logger} from './utils/logger.js'
 
 const PORT = process.env.PORT || 5000
+
+connectDB();
 
 const server = app.listen(PORT , ()=>{
     logger.info(`app is listining on port : ${PORT}`)
